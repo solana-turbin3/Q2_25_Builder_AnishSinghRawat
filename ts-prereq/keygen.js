f148986039a84e46f4ee7e5453a52a750873925f
ANISH-SR <rawatanish08@gmail.com>
2025-04-10 13:53:28 +0530

    dist bug

--- ts-prereq/keygen.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var web3_js_1 = require("@solana/web3.js");
-var kp = web3_js_1.Keypair.generate();
-console.log("You've generated a new Solana wallet: ".concat(kp.publicKey.toBase58()));
-console.log("[".concat(kp.secretKey, "]"));
