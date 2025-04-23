69a0296344e53d8b564b5f9b53324906f5a7bb43
ANISH-SR <rawatanish08@gmail.com>
2025-04-23 15:23:58 +0530

    temp measures XD

--- vault/programs/vault/src/lib.rs
+++ vault/programs/vault/src/lib.rs
@@ -1,10 +1,11 @@
 #![allow(unexpected_cfgs)]
 use anchor_lang::{prelude::*, system_program::{Transfer, transfer}};
+declare_id!("2wqq41oHbVmc5Q9bxbUXLSXLmtpqc788UwDM4N1XvY7U");
 
-declare_id!("2wqq41oHbVmc5Q9bxbUXLSXLmtpqc788UwDM4N1XvY7U");        //Specifies the program's on-chain address
-
-#[program]                                                          //Specifies the module containing the program’s instruction logic
+#[program]
 pub mod vault {
+    use std::u64;
+
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
@@ -19,16 +20,15 @@ pub mod vault {
         ctx.accounts.withdraw(amount)
     }
 
-    pub fn close(ctx: Context<CloseAccount>) -> Result<()> {
+    pub fn close(ctx: Context<Close>) -> Result<()> {
         ctx.accounts.close()
     }
 }
 
 
-//Initialize
-#[derive(Accounts)]                                                                 //Applied to structs to indicate a list of accounts required by an instruction
-pub struct Initialize<'info> {          // 'info -> lifetime
-    #[account(mut)]                     //constraints                               
+#[derive(Accounts)]                 //indicates list of accs required by an instruction
+pub struct Initialize<'info> {
+    #[account(mut)]                 //Account Constraints
     pub signer: Signer<'info>,
 
     #[account(
@@ -36,119 +36,119 @@ pub struct Initialize<'info> {          // 'info -> lifetime
         payer = signer,
         seeds = [b"state", signer.key().as_ref()],
         bump,
-        space = VaultState::INIT_SPACE
+        space = 8 + VaultState::INIT_SPACE
     )]
-    pub vault_state: Account<'info, VaultState>,               //not a system account -> used for deriving both of the pdas
-
+    pub vault_state: Account<'info, VaultState>,
 
     #[account(
-        seeds = [b"vault", vault_state.key().as_ref()],
-        bump,
+        seeds = [vault_state.key().as_ref()],
+        bump
     )]
-    pub vault: SystemAccount<'info>,                           //system account
-    pub system_program: Program<'info, System>,
+    pub vault: SystemAccount<'info>,
+
+    pub system_program: Program<'info, System>
 }
-
-impl<'info> Initialize<'info> {
-    pub fn initialize(&mut self, bumps: &InitializeBumps)-> Result<()> {
-        self.vault_state.vault_bump = bumps.vault;
-        self.vault_state.state_bump = bumps.vault_state;
-
-        Ok(())
-    }
-}
-
-//Payment - Deposit + Withdraw
 
 #[derive(Accounts)]
 pub struct Payment<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
-    
-    #[account(
-        seeds = [b"state", signer.key().as_ref()],
-        bump = vault_state.state_bump,
 
+    #[account(
+        mut,
+        seeds = [b"state", signer.key().as_ref()],
+        bump = vault_state.state_bump
+    )]
+    pub vault_state: Account<'info, VaultState>, 
+
+    #[account(
+        mut,
+        seeds = [vault_state.key().as_ref()],
+        bump = vault_state.vault_bump
+    )]
+    pub vault: SystemAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Close<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        close = signer,
+        seeds = [b"state", signer.key().as_ref()],
+        bump = vault_state.state_bump
     )]
     pub vault_state: Account<'info, VaultState>,
 
     #[account(
         mut,
-        seeds = [b"vault", vault_state.key().as_ref()],         //we used vault state to derive-vault
-        bump = vault_state.vault_bump,
+        seeds = [b"state", vault_state.key().as_ref()],
+        bump = vault_state.vault_bump
     )]
-    pub vault: SystemAccount<'info>,     
-    pub system_program: Program<'info, System>,
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>
+}
 
+
+impl<'info> Initialize<'info> {
+    pub fn initialize(&mut self, bumps: &InitializeBumps) -> Result<()> {
+        self.vault_state.state_bump = bumps.vault_state;
+        self.vault_state.vault_bump = bumps.vault;
+
+        // self.vault_state.set_inner(VaultState { vault_bump: bumps.vault, state_bump: bumps.vault_state });
+        Ok(())
+    }
 }
 
 impl<'info> Payment<'info> {
     pub fn deposit(&mut self, amount: u64) -> Result<()> {
-
         let cpi_program = self.system_program.to_account_info();
-        let cpi_accounts = Transfer {
+        let cpi_account = Transfer {
             from: self.signer.to_account_info(),
-            to: self.vault.to_account_info()
+            to: self.vault.to_account_info(),
         };
-        
-        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_account);
         transfer(cpi_ctx, amount)
     }
-
     pub fn withdraw(&mut self, amount: u64) -> Result<()> {
-
         let cpi_program = self.system_program.to_account_info();
-        let cpi_accounts = Transfer {
+        let cpi_account = Transfer {
             from: self.vault.to_account_info(),
             to: self.signer.to_account_info(),
         };
 
         let seeds = &[
-            b"vault",
+            // b"vault",
             self.vault_state.to_account_info().key.as_ref(),
-            &[self.vault_state.vault_bump],
+            &[self.vault_state.vault_bump]
         ];
+
         let signer_seeds = &[&seeds[..]];
-        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_account, signer_seeds);
         transfer(cpi_ctx, amount)
     }
 }
 
-
-#[derive(Accounts)]
-pub struct CloseAccount<'info> {
-    #[account(mut)]
-    pub signer: Signer<'info>,
-    
-    #[account(
-        mut,
-        seeds = [b"state", signer.key().as_ref()],
-        bump = vault_state.state_bump,
-        close = signer
-    )]
-    pub vault_state: Account<'info, VaultState>,
-
-    #[account(
-        seeds = [b"vault", vault_state.key().as_ref()],         //we used vault state to derive-vault
-        bump = vault_state.vault_bump,
-    )]
-    pub vault: SystemAccount<'info>,     
-    pub system_program: Program<'info, System>,
-}
-
-impl<'info> CloseAccount<'info> {
+impl<'info> Close<'info> {
     pub fn close(&mut self) -> Result<()> {
         let cpi_program = self.system_program.to_account_info();
         let cpi_accounts = Transfer {
             from: self.vault.to_account_info(),
-            to: self.signer.to_account_info(),
+            to: self.signer.to_account_info()
         };
 
         let seeds = &[
-            b"vault",
+            // b"vault",
             self.vault_state.to_account_info().key.as_ref(),
-            &[self.vault_state.vault_bump],
+            &[self.vault_state.vault_bump]
         ];
+
         let signer_seeds = &[&seeds[..]];
         let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
         transfer(cpi_ctx, self.vault.lamports())
@@ -156,13 +156,12 @@ impl<'info> CloseAccount<'info> {
 }
 
 
-#[account]                                                                      //Applied to structs to create custom account types for the program
+#[account]
 pub struct VaultState {
     pub vault_bump: u8,
     pub state_bump: u8,
 }
 
-
 impl Space for VaultState {
-    const INIT_SPACE: usize = 8 + 1 + 1;        //8 -> anchor discriminator
+    const INIT_SPACE: usize = 1 + 1;
 }
--- vault/tests/vault.ts
+++ vault/tests/vault.ts
@@ -5,113 +5,105 @@ import { LAMPORTS_PER_SOL, PublicKey, SystemProgram } from "@solana/web3.js";
 import { confirmTransaction } from "@solana-developers/helpers";
 import { assert } from "chai";
 
-console.log(JSON.stringify(Program.fetchIdl, null, 2));
-
 describe("vault", () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
-  
   anchor.setProvider(provider);
 
-  // the program is accessible on the anchor.workspace attribute
-  const program = anchor.workspace.Vault as Program<Vault>;
+  const program = anchor.workspace.vault as Program<Vault>;
 
   const connection = provider.connection;
 
   let signer;
-  let vault;
   let vault_state;
   let bump;
+  let vault;
 
-  before(async () => {
+
+  before(
+    async () => {
       signer = anchor.web3.Keypair.generate();
 
       [vault_state, bump] = PublicKey.findProgramAddressSync([
-          Buffer.from("state"),
-          signer.publicKey.toBuffer(),
+        Buffer.from("state"),
+        signer.publicKey.toBuffer(),        //pda jisse derive hua hai, uska toBuffer karna hai
       ], program.programId);
-      
+
       [vault, bump] = PublicKey.findProgramAddressSync([
-        Buffer.from("vault"),
         vault_state.toBuffer(),
       ], program.programId);
 
       await airdrop(connection, signer.publicKey, 5);
-      // airdrop some SOL to the vault so it does not disappear (make it rent exempt)
 
-      // let rent_exemption_amount = await calculateRentExemption(connection, vault);
+      // to make the vault rent-exempt
       await airdrop(connection, vault, 1);
-  });
+    }  
+  );
 
   it("Is initialized!", async () => {
+    // Add your test here.
     const tx = await program.methods
-      .initialize()
-      .accounts({
-        signer: signer.publicKey,
-        vault_state: vault_state,
-        vault: vault,
-        systemProgram: SystemProgram.programId,
-      })
-      .signers([signer])
-      .rpc();
-    console.log("✅ Your Initialization transaction signature", tx);
+    .initialize()
+    .accounts({
+      signer: signer.publicKey,
+      vaultState: vault_state,
+      vault: vault,
+      systemProgram: SystemProgram.programId,
+    })
+    .signers([signer])
+    .rpc();
+    console.log("Your initialize transaction signature", tx);
   });
 
   it("Is Deposited!", async () => {
-    const tx  = await program.methods
-      .deposit(new BN(1_000_000_000))
-      .accounts({
-        signer: signer.publicKey,
-        vault_state: vault_state,
-        vault: vault,
-        systemProgram: SystemProgram.programId
-      })
-      .signers([signer])
-      .rpc();
-    console.log("✅ Your Deposit transaction signature", tx);
-  
-    // assert that the vault balance increase after the transaction
-    let vaultBalance = await getBalance(connection, vault);
-    assert.equal(vaultBalance, 2 * 1_000_000_000);
+    const tx = await program.methods
+    .deposit(new BN(1_000_000_000))
+    .accounts({
+      signer: signer.publicKey,
+      vaultState: vault_state,
+      vault: vault,
+      systemProgram: SystemProgram.programId
+    })
+    .signers([signer])
+    .rpc();
+    console.log("Your deposit transaction signature", tx);
 
-    // assert that the signer balance decrease after the transaction
-    // let signerBalance = await getBalance(connection, signer.publicKey); 
-    // assert.isTrue(signerBalance <= 5);
-  });
 
-  it("Is Withdraw!", async () => {
-    const tx = program.methods
-      .withdraw(new BN(1_000_000_000))
-      .accounts({
-        signer: signer.publicKey,
-        vault_state: vault_state,
-        vault: vault,
-        systemProgram: SystemProgram.programId
-      })
-      .signers([signer])
-      .rpc();
+    //checks
+    let vault_balance = await getBalance(connection, vault);
+    assert.equal(vault_balance, 2 * 1_000_000_000);   //provided by chai
+    
+    let signerBalance = await getBalance(connection, signer.publicKey); 
+    assert.isTrue(signerBalance >= 5);
+  })
 
-      let vaultBalance = await getBalance(connection, vault);
-      // assert that the vault balance increase after the transaction
-      assert.equal(vaultBalance, 2 * LAMPORTS_PER_SOL);
+  it("Is Withdrawn!", async () => {
+    const tx = await program.methods
+    .withdraw(new BN(1))
+    .accounts({
+      signer: signer.publicKey,
+      vaultState: vault_state,
+      vault: vault,
+      systemProgram: SystemProgram.programId
+    })
+    .signers([signer])
+    .rpc();
 
-      // assert that the signer balance decrease after the transaction
-      // let signerBalance = await getBalance(connection, signer.publicKey); 
-      // assert.isTrue(signerBalance >= 4.9 * LAMPORTS_PER_SOL);
-  });
+    console.log("Your withdraw transaction signature", tx);
+  })
 
   it("Is Closed!", async () => {
-    const tx = program.methods
-      .close()
-      .accounts({
-        signer: signer.publicKey,
-        vault_state: vault_state,
-        vault: vault,
-        systemProgram: SystemProgram.programId
-      })
-      .signers([signer])
-      .rpc();
-  });
+    const tx = await program.methods
+    .close()
+    .accounts({
+      signer: signer.publicKey,
+      vaultState: vault_state,
+      vault: vault,
+      systemProgram: SystemProgram.programId
+    })
+    .signers([signer])
+    .rpc;
+  })
 });
 
 async function airdrop(connection, address: PublicKey, amount: number) {
@@ -127,22 +119,6 @@ async function airdrop(connection, address: PublicKey, amount: number) {
   console.log("✅ Tx Signature: ", confirmedAirdrop);
 
   return confirmedAirdrop;
-}
-
-async function calculateRentExemption(connection: anchor.web3.Connection, address: PublicKey) {
-  let accountInfo = await connection.getAccountInfo(address);
-
-  let accountSize;
-
-  if (accountInfo === null) {
-    accountSize = 1000;
-  } else {
-    accountSize = accountInfo.data.length;
-  }
-  
-  const rentExemptionAmount = await connection.getMinimumBalanceForRentExemption(accountSize);
-
-  return rentExemptionAmount;
 }
 
 async function getBalance(connection: anchor.web3.Connection, address: PublicKey) {
